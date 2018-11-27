11864ed9d9ce2f9c93323cf32b6ff9cee2429b7e
SuprioBandyopadhyay <supriob151277@gmail.com>
2018-11-27 17:15:59 +0530

    My First Commit

---
+++ Project2/src/Package2/Class2.java
@@ -0,0 +1,15 @@
+package Package2;
+
+public class Class2 {
+
+	public static void main(String[] args) 
+		
+	{
+		System.out.println("Hallo World");
+        System.out.println("hi");
+        System.out.println("Suprio");
+        System.out.println("selenium");
+        System.out.println("tutorials");
+	}
+
+}
